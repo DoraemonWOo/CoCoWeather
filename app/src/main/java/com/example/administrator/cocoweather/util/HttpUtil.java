53d8dec59eb05ccf9ff3527e8e9e7bd6cf6a8e0b
DoraemonWOo <994626660@qq.com>
2017-04-17 20:07:20 +0800

    解析处理省份数据，新建省份选择的界面布局

---
+++ app/src/main/java/com/example/administrator/cocoweather/util/HttpUtil.java
@@ -0,0 +1,16 @@
+package com.example.administrator.cocoweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Administrator on 2017/4/16.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client =new OkHttpClient();
+        Request request =new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
